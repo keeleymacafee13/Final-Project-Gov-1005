1437ba3921c072161468c60fdbb4f77deabaa9d2
Keeley MacAfee <keeleymacafee@Keeleys-MacBook.local>
2018-12-05 05:57:26 -0500

    final commit/submission including app and markdown updates and data commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Final-Project-Gov-1005
+
+The United States is facing a food emergency. Using data from the food expenditure series published by the U.S. Department of Agriculture (USDA), I examine the per capita trends in spending on food and food availability over the past two decades. I use selected food groups (dairy, grains, fish, etc.) and selected retailing locations (grocery stores, convenience stores, etc.) with the hopes that this visualization can be extrapolated out to what is occurring in the food industry more generally.
+
+Of course, increased spending over time can be attributed to inflation, tax laws, etc. The purpose of this project is not to examine those causes, but to rather visualize the gap between decreasing food availability and increasing food expenditure. This project also provides a glimpse of which consumer channels are gaining popularity more rapidly and which have plateaued, as new purchasing mechanisms, like eCommerce and meal delivery, have become more accessible to consumers. 
+
+My deductions are twofold. First, the USDA data does not account for highly processed goods (soups, frozen dinners, etc.). So, although food availability is going down while expenditure (particularly at convenience stores) is going up in this case, that is likely due to the fact that Americans are relying on and purchasing more heavily processed foods. Second, the way Americans purchase food is changing. Delivery services, like monthly box subscriptions or online orders, are replacing traditional consumer avenues, such as Costco and other mass merchandisers. It seems as though we are moving towards a more processed, more automated food sphere. 
+
+Data sources:
+https://www.ers.usda.gov/data-products/food-expenditure-series/
+https://www.ers.usda.gov/data-products/food-availability-per-capita-data-system/
+https://fred.stlouisfed.org/series/POPTOTUSA647NWDB
+
+Link to my shiny:
+https://keeley-macafee.shinyapps.io/final-app/
+
--- final-app/app.R
+++ final-app/app.R
@@ -3,62 +3,75 @@
 library(shiny)
 library(tidyverse)
 
-exp <- read_rds("expenditures.rds")
-exp_no_taxes_tips <- read_rds("expenditures_no_taxes_tips.rds")
-tot_indiv <- read_rds("total_indiv.rds")
-food <- read_rds("food.rds")
-indiv_tips_taxes <- read_rds("indiv_tips_taxes.rds")
 indiv_food1 <- read_rds("indiv_food.rds")
-total_indiv <- read_rds("total_indiv.rds")
-indiv_availability <- read_rds("indiv_availability.rds")
 availability_types <- read_rds("availability_types.rds")
 
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- fluidPage(
-  tabsetPanel(
-    tabPanel("Choices", fluid = TRUE,
-               sidebarPanel(
-                 selectInput(inputId = "y", 
-                             label = "Select Location:", 
-                             choices = c("Grocery Stores", "Convenience Stores", "Mass Merchandisers", 
-                                         "Mail Order and Home Delivery","Home Production and Donations",
-                                         "Full Service Restaurants",
-                                         "Hotels and Motels", 
-                                         "Schools and Colleges"),
-                             selected = "Mass Merchandisers"),
-                 selectInput(inputId = "z",
-                              label = "Select Location:", 
-                              choices = c("Grocery Stores", "Convenience Stores", "Mass Merchandisers", 
-                                          "Mail Order and Home Delivery","Home Production and Donations",
-                                          "Full Service Restaurants",
-                                          "Hotels and Motels", 
-                                          "Schools and Colleges"),
-                             selected = "Mail Order and Home Delivery"))
-    ) ,
-    
-    
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("About", htmlOutput("about")),
-                  tabPanel("Plot 1", plotOutput("Plot1")),
-                  tabPanel("Plot 2", plotOutput("Plot2")),
-                  tabPanel("Plot 3", plotOutput("Plot3")))
-    )
-  )
-    )
+  titlePanel("Visualizing Food Expenditure and Availability in the U.S."),
   
+  tabsetPanel(
+    tabPanel("", fluid = TRUE,
+      sidebarLayout(
+        sidebarPanel(
+              selectInput(inputId = "y", 
+                  label = "Select Location:", 
+                  
+                  # I chose the most relevant retailing locations, as there were many possible selections.
+                  
+                  choices = c("Grocery Stores", "Convenience Stores", "Mass Merchandisers", 
+                              "Mail Order and Home Delivery","Home Production",
+                              "Full Service Restaurants",
+                              "Hotels and Motels", 
+                              "Schools and Colleges"),
+                  selected = "Grocery Stores"),
+              
+              # I could not figure out how to change the sidebarPanel per tab, so I added some text to 
+              # clarify that the drop-down menu is specifically for the food expenditure plot. 
+              
+              tags$h6(helpText("Select retail location 
+                               to visualize food expenditure per capita 
+                               over time."))),
+      
+      
+      
+              
+      
+      mainPanel(
+        tabsetPanel(type = "tabs",
+                    tabPanel("About", htmlOutput("about")),
+                    tabPanel("Food Availability", plotOutput("Plot1")),
+                    tabPanel("Food Expenditures", plotOutput("Plot2")))
+)))))
 
 
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
   
+  # I wanted to provide further brief clarification that the first plot on food availability
+  # is not reactive, but the second plot on food expenditure is. 
   
-
+  output$about <- renderText({
+    "The first tab statically displays food availability in pounds per capita over time. 
+    I divided the variables into perishable and imperishable to aid visualization.
+    
+    In the second tab, use the drop-down menu to select a food retailing location and visualize
+    American expenditure patterns at the selected location."
+  })
+  
+  
   output$Plot1 <- renderPlot({
+    
+    ggplot(availability_types, aes(x = year, y = total, color = category)) + geom_point() + geom_smooth() +
+      ggtitle("Yearly Food Availability in the United States: Pounds Per Capita")
+    
+  })
+
+  output$Plot2 <- renderPlot({
     
     
       ggplot(indiv_food1, aes(x = year,  y = switch(input$y,
@@ -66,63 +79,20 @@ server <- function(input, output) {
                                                    "Convenience Stores" = indiv_conv,
                                                    "Mass Merchandisers" = indiv_mass, 
                                                    "Mail Order and Home Delivery" = indiv_mail,
-                                                   "Home Production and Donations" = indiv_home,
+                                                   "Home Production" = indiv_home,
                                                    "Full Service Restaurants" = indiv_rest,
                                                    "Hotels and Motels" = indiv_hotel,
-                                                   "Schools and Colleges" = indiv_school), color = "red")) +
+                                                   "Schools and Colleges" = indiv_school))) +
         geom_point() +
-        geom_smooth() +
-        geom_smooth(aes(y = switch(input$z,
-                                   "Grocery Stores" = indiv_grocery, 
-                                   "Convenience Stores" = indiv_conv,
-                                   "Mass Merchandisers" = indiv_mass, 
-                                   "Mail Order and Home Delivery" = indiv_mail,
-                                   "Home Production and Donations" = indiv_home,
-                                   "Full Service Restaurants" = indiv_rest,
-                                   "Hotels and Motels" = indiv_hotel,
-                                   "Schools and Colleges" = indiv_school), color = "blue")) +
-        ggtitle("Average Expenditure on Food Per Individual in the U.S.") +
+        geom_smooth(color = "red") +
+        ggtitle("Average Food Expenditure in the United States: Dollars Per Capita") +
         xlab("Year") +
         ylab("Expenditures (in Dollars)")})
-    
-   
-    
 
-    
-  output$Plot2 <- renderPlot({ 
-    ggplot(indiv_tips_taxes, aes(x = year, y = switch(input$y,
-                                                      "Grocery Stores" = indiv_t_grocery, 
-                                                      "Convenience Stores" = indiv_t_conv,
-                                                      "Mass Merchandisers" = indiv_t_mass, 
-                                                      "Mail Order and Home Delivery" = indiv_t_mail,
-                                                      "Home Production and Donations" = indiv_t_home,
-                                                      "Full Service Restaurants" = indiv_t_rest,
-                                                      "Hotels and Motels" = indiv_t_hotel,
-                                                      "Schools and Colleges" = indiv_t_school), color = "red")) + geom_point() +
-      geom_smooth()  + 
-      geom_smooth(aes(y = switch(input$z,
-                                 "Grocery Stores" = indiv_t_grocery, 
-                                 "Convenience Stores" = indiv_t_conv,
-                                 "Mass Merchandisers" = indiv_t_mass, 
-                                 "Mail Order and Home Delivery" = indiv_t_mail,
-                                 "Home Production and Donations" = indiv_t_home,
-                                 "Full Service Restaurants" = indiv_t_rest,
-                                 "Hotels and Motels" = indiv_t_hotel,
-                                 "Schools and Colleges" = indiv_t_school), color = "blue")) +
-      ggtitle("Tax and Tips Per Individual in the U.S.") +
-      xlab("Year") +
-      ylab("Tip and Tax Amount (in Dollars)")
-      
-  })
-
-  output$Plot3 <- renderPlot({
-    
-    ggplot(availability_types, aes(x = year, y = total, color = category)) + geom_point() + geom_smooth()
-    
-    
-  })
-
-  
+  # At first, I made the expenditure plot comparitive, so the user could select two different retailing
+  # locations and visualize the difference. However, I felt this was too confusing, especially with the 
+  # two lines in the food availability plot. Also, grocery stores are so far above all of the other locations
+  # that it was difficult to see the actual trends in a comparitive context. 
 }
 
 # Run the application 
